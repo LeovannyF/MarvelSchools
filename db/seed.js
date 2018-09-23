196772422da574c25456d37532c69b1035e15a92
Your Name <fernandez.Leovanny@gmail.com>
2018-09-23 10:49:23 -0400

    seed file created

---
+++ db/seed.js
@@ -0,0 +1,50 @@
+const studentData = [
+  {
+    firstName: 'Thor',
+    lastName: 'Avenger',
+    gpa: 3.1
+  },
+  {
+    firstName: 'Tony ',
+    lastName: 'Stark',
+    gpa: 5
+  },
+  {
+    firstName: 'Hulk ',
+    lastName: 'Smash',
+    gpa: 2.0
+  },
+  {
+    firstName: 'Black',
+    lastName: 'Panther',
+    gpa: 3.98
+  },
+  {
+    firstName: 'Captian ',
+    lastName: 'America',
+    gpa: 3.6
+  }
+]
+
+const schoolData = [
+  {
+    name:'Bronx School',
+    Address: '112 Bronx avenue',
+    moto: 'Make the most of now.'
+  },
+  {
+    name:'Brooklyn School',
+    Address: '245 Brooklyn avenue',
+    moto: 'Success. Its a mind game.'
+  },
+  {
+    name:'Manhattan School',
+    Address: '80 Manhattan avenue',
+    moto: 'Impossible is nothing.'
+  }
+]
+
+module.exports = {
+  studentData,
+  schoolData
+}
