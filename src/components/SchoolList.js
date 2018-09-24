a935c271e9ffbe9c944698f28053d18fd0c91fea
Your Name <fernandez.Leovanny@gmail.com>
2018-09-24 14:01:20 -0400

    latest commit

---
+++ src/components/SchoolList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+
+const SchoolList = ({schoolList}) => {
+  return (
+
+  )
+}
+
+const mapStateToProps = ({schoolList}) => {
+  return {
+    schoolList
+  }
+}
+
+export default 
